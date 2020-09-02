c1465b42cb28337ffa21a2fe611b25d193ae4e88
rapixar <cudraphael@gmail.com>
2020-09-02 22:25:56 +0100

    repo initialization

---
+++ src/components/Grid/GridContainer.js
@@ -0,0 +1,46 @@
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const styles = {
+  grid: {
+    marginRight: "-15px",
+    marginLeft: "-15px",
+    width: "auto"
+  }
+};
+
+const useStyles = makeStyles(styles);
+
+export default function GridContainer(props) {
+  const classes = useStyles();
+  const { children, className, color, ...rest } = props;
+  return (
+    <Grid container {...rest} className={classes.grid + " " + className}>
+      {children}
+    </Grid>
+  );
+}
+
+GridContainer.defaultProps = {
+  className: ""
+};
+
+GridContainer.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  color: PropTypes.oneOf([
+    "primary",
+    "info",
+    "success",
+    "warning",
+    "danger",
+    "transparent",
+    "white",
+    "rose",
+    "dark",]),
+};
---
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -0,0 +1,63 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Chat";
+import VerifiedUser from "@material-ui/icons/VerifiedUser";
+import Fingerprint from "@material-ui/icons/Fingerprint";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import InfoArea from "components/InfoArea/InfoArea.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProductSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer color="rose" justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Let{"'"}s talk services</h2>
+          <h5 className={classes.description}>
+            Providing premium management services to our clients around the globe, our horizons are as wide as the touching distance of the sun in our Solar System
+          </h5>
+        </GridItem>
+      </GridContainer>
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Clients smile, we smile"
+              description="We ensure we always leave a smile on the faces of our customers. Providing quality customer service for our esteemed clients round the clock."
+              icon={Chat}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Authenticity"
+              description="Everyone likes an assurance epecially when it comes to investing money. Wildra gives you that assurance through well structured management techniques. Now, you can live and breathe."
+              icon={VerifiedUser}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Authenticity"
+              description="Get secured from scammers and fradulent agents when you shield your properties with WildraSecure, a safer way to toggle the locks of your properties"
+              icon={Fingerprint}
+              iconColor="danger"
+              vertical
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
